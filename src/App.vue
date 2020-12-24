7636459610bb08a3387ff15b739dd745136456f0
Kam <kamyshova.ea@yandex.com>
2020-12-24 19:36:25 +0300

    Pizza is ready

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,41 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+   <app-pizza v-bind:pizzaName='pizzaName' v-bind:pizzaSize='pizzaSize' v-bind:pizzaPrice='pizzaPrice'>
+<h1 slot="n1">Покупайте пиццу</h1>
+<p>Состав: мука, соль, перец, томатная паста, колбаса, сыр</p>
+   </app-pizza>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Pizza from './components/Pizza.vue'
 
 export default {
-  name: 'App',
+  name: 'app',
+  data(){
+    return {
+      pizzaName: 'ШПицца от Школы программистов',
+      pizzaPrice: 600,
+      pizzaSize: 30
+    }
+  },
   components: {
-    HelloWorld
-  }
+    appPizza: Pizza
+  } 
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+body {
+    background: rgb(238,174,202);
+    background: radial-gradient(circle, rgba(238,174,202,1) 0%, rgba(148,187,233,1) 100%);
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    min-height: 100vh;
+    margin: 0;
+    padding: 0;
+    font-family: sans-serif;
+    font-size: 18px;
 }
 </style>
---
+++ src/components/Pizza.vue
@@ -0,0 +1,15 @@
+<template>
+    <div>
+        <slot name="n1"></slot>
+        <h2>Название пиццы: {{pizzaName}}</h2>
+        <p>Размер: {{pizzaSize}}см</p>
+        <p>Цена: {{pizzaPrice}}$</p>
+        <slot></slot>
+    </div>
+</template>
+
+<script>
+export default {
+    props: ['pizzaName','pizzaPrice', 'pizzaSize']   
+}
+</script>
